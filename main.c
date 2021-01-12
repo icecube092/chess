faf61cde74a0016fb6b1c9d91cd4e979e117d08c
icecube092 <icecube092@gmail.com>
2021-01-12 16:11:49 +0300

    - вывод поля - валидация позиции и стороны - решена проблема с конями - иконка пустой ячейки - убраны лишние поля - нужно сделать валидацию фигур

--- main.c
+++ main.c
@@ -2,11 +2,91 @@
 #include <stdlib.h>
 #include "shapes.c"
 
+struct shape (*board)[8];
+char side;
+int done;
 
-void main(){
-    struct shape (*board)[8];
+void show_board();
+void define_side(int turn);
+char validate();
+
+int main(){
     board = malloc(sizeof(struct shape) * 64);
     make_shapes(board);
-    printf("%d %c\n", board[3][7].isalive, board[7][7].color);
-    printf("%ld\n", sizeof(board));
+
+//    printf("%d", xx);
+    for (int turn = 1; turn; turn++){
+        int sx = -1;
+        int sy = -1;
+        int fx = -1;
+        int fy = -1;
+        done = 0;
+        define_side(turn);
+        printf("Turn %d. %c move.\n", turn, side);
+        while (!done){
+            show_board();
+            done = validate(&sx, &sy, &fx, &fy);
+        }
+    }
+    free(board);
+}
+
+char validate_turn(int sx, int sy, int fx, int fy){
+    if (board[sx][sy].color == side && board[fx][fy].color != side){
+        switch (board[sx][sy].shapetype){
+            case
+        }
+    }
+    return 0;
+}
+
+char validate_position(int x, int y){
+    if (0 <= x && x <= 7 && 0 <= y && y <= 7){
+        return 1;
+    }
+    return 0;
+}
+
+char validate(int sx, int sy, int fx, int fy){
+    for (;;){
+        printf("Choose your shape row: \n");
+        scanf("%d", &sx);
+        printf("Choose your shape column: \n");
+        scanf("%d", &sy);
+        if (validate_position(sx, sy)){
+            printf("Choose finish row: \n");
+            scanf("%d", &fx);
+            printf("Choose finish column: \n");
+            scanf("%d", &fy);
+            if (validate_position(fx, fy)){
+                if (validate_turn(sx, sy, fx, fy)){
+                    return 1;
+                }
+            }
+        }
+        printf("Wrong position, enter again\n");
+    }
+}
+
+void define_side(int turn){
+    switch (turn % 2){
+        case 0:
+            side = BLACK;
+            break;
+        case 1:
+            side = WHITE;
+    }
+}
+
+void show_board(){
+    printf("-----------------------------\n");
+    for (int r = 0; r < 8; r++){
+        printf("%d | ", r);
+        for (int n = 0; n < 8; n++){
+            printf("%s ", board[r][n].view);
+        }
+        printf("|\n");
+    }
+    printf("-----------------------------\n");
+    printf("  | 0  1  2  3  4  5  6  7  |\n");
 }
--- shapes.c
+++ shapes.c
@@ -5,10 +5,8 @@ struct shape{
     char updown;
     char leftright;
     char diag;
-//    unsigned char shapetype;
+    unsigned char shapetype;
     unsigned char number;
-    unsigned char posx; // 1-8
-    unsigned char posy; // 1-8
     char *view;
 };
 
@@ -76,44 +74,39 @@ char *get_view(char color, char shape){
     return s;
 }
 
+void make_horses(){
+    bhorse_1.color = bhorse_2.color = BLACK;
+    bhorse_1.view = bhorse_2.view = get_view(BLACK, HORSE);
+
+    whorse_1.color = whorse_2.color = WHITE;
+    whorse_1.view = whorse_2.view = get_view(WHITE, HORSE);
+
+    bhorse_1.shapetype = bhorse_2.shapetype = whorse_1.shapetype = whorse_2.shapetype = HORSE;
+}
+
 void make_pawns(){
     bpawn_1.color = bpawn_2.color = bpawn_3.color = bpawn_4.color = bpawn_5.color = bpawn_6.color = bpawn_7.color = bpawn_8.color = BLACK;
     bpawn_1.updown = bpawn_2.updown = bpawn_3.updown = bpawn_4.updown = bpawn_5.updown = bpawn_6.updown = bpawn_7.updown = bpawn_8.updown = 1;
     bpawn_1.view = bpawn_2.view = bpawn_3.view = bpawn_4.view = bpawn_5.view = bpawn_6.view = bpawn_7.view = bpawn_8.view = get_view(BLACK, PAWN);
-    bpawn_1.posy = bpawn_2.posy = bpawn_3.posy = bpawn_4.posy = bpawn_5.posy = bpawn_6.posy = bpawn_7.posy = bpawn_8.posy = 1;
 
     wpawn_1.color = wpawn_2.color = wpawn_3.color = wpawn_4.color = wpawn_5.color = wpawn_6.color = wpawn_7.color = wpawn_8.color = WHITE;
     wpawn_1.updown = wpawn_2.updown = wpawn_3.updown = wpawn_4.updown = wpawn_5.updown = wpawn_6.updown = wpawn_7.updown = wpawn_8.updown = 1;
     wpawn_1.view = wpawn_2.view = wpawn_3.view = wpawn_4.view = wpawn_5.view = wpawn_6.view = wpawn_7.view = wpawn_8.view = get_view(WHITE, PAWN);
 
-    wpawn_1.posy = wpawn_2.posy = wpawn_3.posy = wpawn_4.posy = wpawn_5.posy = wpawn_6.posy = wpawn_7.posy = wpawn_8.posy = 6;
-
-    bpawn_1.posx = bpawn_1.number = wpawn_1.number = wpawn_1.posx = 0;
-    bpawn_2.posx = bpawn_2.number = wpawn_2.number = wpawn_2.posx = 1;
-    bpawn_3.posx = bpawn_3.number = wpawn_3.number = wpawn_3.posx = 2;
-    bpawn_4.posx = bpawn_4.number = wpawn_4.number = wpawn_4.posx = 3;
-    bpawn_5.posx = bpawn_5.number = wpawn_5.number = wpawn_5.posx = 4;
-    bpawn_6.posx = bpawn_6.number = wpawn_6.number = wpawn_6.posx = 5;
-    bpawn_7.posx = bpawn_7.number = wpawn_7.number = wpawn_7.posx = 6;
-    bpawn_8.posx = bpawn_8.number = wpawn_8.number = wpawn_8.posx = 7;
-
+    bpawn_1.shapetype = bpawn_2.shapetype = bpawn_3.shapetype = bpawn_4.shapetype = bpawn_5.shapetype = bpawn_6.shapetype =
+    bpawn_7.shapetype = bpawn_8.shapetype = wpawn_1.shapetype = wpawn_2.shapetype = wpawn_3.shapetype = wpawn_4.shapetype =
+    wpawn_5.shapetype = wpawn_6.shapetype = wpawn_7.shapetype = wpawn_8.shapetype = PAWN;
 }
 
 void make_elefs(){
     belef_1.color = belef_2.color = BLACK;
     belef_1.updown = belef_2.updown = 8;
     belef_1.leftright = belef_2.leftright = 8;
-    belef_1.posy = belef_2.posy = 0;
-    belef_1.posx = 0;
-    belef_2.posx = 7;
     belef_1.view = belef_2.view = get_view(BLACK, ELEF);
 
     welef_1.color = welef_2.color = WHITE;
     welef_1.updown = welef_2.updown = 8;
     welef_1.leftright = welef_2.leftright = 8;
-    welef_1.posy = welef_2.posy = 7;
-    welef_1.posx = 0;
-    welef_2.posx = 7;
     welef_1.view = welef_2.view = get_view(WHITE, ELEF);
 
     belef_1.number = welef_1.number = 0;
@@ -123,16 +116,10 @@ void make_elefs(){
 void make_rooks(){
     brook_1.color = brook_2.color = BLACK;
     brook_1.diag = brook_2.diag = 8;
-    brook_1.posy = brook_2.posy = 0;
-    brook_1.posx = 2;
-    brook_1.posx = 5;
     brook_1.view = brook_2.view = get_view(BLACK, ROOK);
 
     wrook_1.color = wrook_2.color = WHITE;
     wrook_1.diag = wrook_2.diag = 8;
-    wrook_1.posy = wrook_2.posy = 7;
-    wrook_1.posx = 2;
-    wrook_1.posx = 5;
     wrook_1.view = wrook_2.view = get_view(WHITE, ROOK);
 
     brook_1.number = wrook_1.number = 0;
@@ -157,15 +144,17 @@ void make_kings(){
     wking.color = WHITE;
     wking.view = get_view(WHITE, KING);
 
-    bking.updown = bking.leftright = bking.diag = wking.updown = wking.leftright = wking.diag = 8;
+    bking.updown = bking.leftright = bking.diag = wking.updown = wking.leftright = wking.diag = 1;
 }
 
 void make_shapes(struct shape (*board)[8]){
+    make_horses();
     make_pawns();
     make_elefs();
     make_rooks();
     make_queens();
     make_kings();
+    empty.view = "--";
     struct shape b[8][8] = {
         welef_1, whorse_1, wrook_1, wking, wqueen, wrook_2, whorse_2, welef_2,
         wpawn_1, wpawn_2, wpawn_3, wpawn_4, wpawn_5, wpawn_6, wpawn_7, wpawn_8,
